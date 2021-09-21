7c0f4831115cba57ca2d2eab2883bf7bf0df3440
Jessica Verduzco <jk.do77@gmail.com>
2021-09-21 17:45:49 -0500

    Length of last word

---
+++ basic/lengthOfLastWord.js
@@ -0,0 +1,35 @@
+// Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+// Example 2:
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+// Example 3:
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+
+var lengthOfLastWord = function(s) {
+    let count = 0
+    let i = s.length-1
+        while(i>= 0){
+        if(s[i] != ' ') count++;
+        else if(count > 0) return count;
+        i--;
+    }
+    
+    return count;
+};
+
+console.log(lengthOfLastWord("flight me to the moon "));
