5bbbdb020a800f043fbcea3e240f56e5f5f9749e
Jessica Verduzco <jk.do77@gmail.com>
2021-09-29 18:22:58 -0500

    Add binary

---
+++ basic/addBinary.js
@@ -0,0 +1,15 @@
+// Given two binary strings a and b, return their sum as a binary string.
+
+// Example 1:
+// Input: a = "11", b = "1"
+// Output: "100"
+
+// Example 2:
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+
+const addBinary = function(a, b) {
+    return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
+};
+
+console.log(addBinary('1010','1011'));
