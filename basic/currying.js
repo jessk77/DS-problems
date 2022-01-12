f752462436520b3f013c9db797ae37e22da2fa3c
Jessica Verduzco <jk.do77@gmail.com>
2022-01-11 19:35:22 -0600

    Currying one line solution

--- basic/currying.js
+++ basic/currying.js
@@ -1,12 +1,5 @@
 /// Can you write code for this function: sum(a)(b)(c)....( n)(). This should return the sum of a+b+c....+n.
 
-const sum = (a) => {
-    return (b) => {
-        if(b){
-            return sum(a+b);
-        }
-        return a;
-    }
-}
+const sum = (a) => (b) => b? sum(a+b) : a;
 
 console.log(sum(1)(2)(3)(4)());
